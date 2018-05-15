94bfd0e93c182dc12f791e0624a524bf900c6671
Boris Bujak <boris.bujak@symphony.is>
2018-05-15 16:13:19 +0200

    Removed specific values

--- Task.py
+++ Task.py
@@ -7,20 +7,20 @@ import os
 
 
 class Task(object):
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         config = configparser.ConfigParser()
         config_file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "zoho.conf")
         config.read(config_file_path)
         self.authtoken =config.get('zoho_parameters', 'authtoken')
         self.user_id = config.get('zoho_parameters', 'user_id')
         self.base_url = "https://projectsapi.zoho.com/restapi/"
-        self.portal_id = "632377220"
-        self.project_id = config.get('zoho_parameters', 'project_id')
+        self.portal_id = config.get('zoho_parameters', 'user_id')
+        self.project_id = config.get('zoho_parameters', 'portal_id')
         self.task_id = config.get('zoho_parameters', 'task_id')
         self.add_task_url = "{}portal/{}/projects/{}/tasks/{}/logs/"\
             .format(self.base_url, self.portal_id, self.project_id, self.task_id)
 
-    def add_task(self, month, days_to_skip):
+    def add_task(self, month, days_to_skip, notes=None):
         today = datetime.date.today()
         year = today.year
         num_days = calendar.monthrange(year, month)[1]
@@ -35,7 +35,7 @@ class Task(object):
                     "owner": self.user_id,
                     "bill_status": "Billable",
                     "hours": 8,
-                    "notes": "development"
+                    "notes": notes
                 }
                 response = requests.post(self.add_task_url, params=params)
                 if response.status_code == 201:
@@ -47,13 +47,21 @@ class Task(object):
 
 if __name__ == "__main__":
     month = None
+    notes = "development"
     days_to_skip = []
     try:
         for item in sys.argv:
-            month = int(item) if month is None else month
-            days_to_skip.append(int(item))
+            if "month=" in item:
+                month = int(item.split("month=")[1])
+            elif "days_to_skip=" in item:
+                days_to_skip = [int(day) for day in (item.split("days_to_skip=")[1]).split(",")]
+            elif "notes=" in item:
+                notes = item.split("notes=")[1]
     except Exception as e:
-        print("Enter valid parameters!\nYou need to enter: Month, Days To Skip\nExample 1: 5\nExample 2: 5 , 1, 2, 3")
+        print("Enter valid parameters!"
+              "\nYou need to enter: Month, Days To Skip (optional), Notes (optional)"
+              "\nExample: month=5"
+              "\nExample: month=5 days_to_skip=1,2,3 notes=development")
 
     if month:
         task = Task()
